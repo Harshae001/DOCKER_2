cd827352ac0a9863f40b308fab583edfa05ad106
Harsha <harsha1ga20ec060@gmail.com>
2024-04-15 14:53:49 +0530

    third number added

--- python_folder/sum.py
+++ python_folder/sum.py
@@ -1,7 +1,9 @@
 def add_numbers():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    return num1 + num2
+    num3 = float(input("Enter the third number: "))
+
+    return num1 + num2 +num3
 
 result = add_numbers()
 print("The sum is this:", result)
